36f98a5f17a6948f03b0c020ac7e594fa3d8514c
RexMa88 <rex.smith98@gmail.com>
2015-09-04 12:17:18 +0800

    Quick Sort

--- QuickSort/main.c
+++ QuickSort/main.c
@@ -7,11 +7,12 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAXSIZE 10
 
-typedef struct SqList{
-    int r[MAXSIZE+1];
+typedef struct{
+    int r[MAXSIZE];
     int length;
 }SqList;
 
@@ -21,8 +22,53 @@ void swap(SqList *L,int i,int j){
     L->r[j] = temp;
 }
 
+int Partition(SqList *L,int low,int high){
+    int pivotKey;
+    pivotKey = L->r[low];
+    while (low < high) {
+        while (low<high && L->r[high]>pivotKey) {
+            high--;
+        }
+        swap(L, low, high);
+        while (low < high && L->r[low]<pivotKey) {
+            low++;
+        }
+        swap(L, low, high);
+    }
+    return low;
+}
+
+void Qsort(SqList *L,int low,int high){
+    int pivot;
+    if(low < high){
+        pivot = Partition(L,low,high);
+        
+        Qsort(L,low,pivot - 1);
+        Qsort(L,pivot + 1,high);
+    }
+}
+
+
+void QuickSort(SqList *L){
+    Qsort(L,1,L->length);
+}
+
 int main(int argc, const char * argv[]) {
     // insert code here...
+    SqList *L = (SqList *)malloc(sizeof(SqList));
+    
+    for (int i = 0; i < 10; i++) {
+        L->r[i] = rand();
+    }
+    
+    L->length = 10;
+    
+    QuickSort(L);
+    
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n",L->r[i]);
+    }
+    
     printf("Hello, World!\n");
     return 0;
 }
